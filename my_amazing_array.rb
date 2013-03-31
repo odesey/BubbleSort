72dcdd5fe2cb89fc7c3a02683f5523d1cd8fdf68
odesey <odesey@gmail.com>
2013-03-31 18:55:49 -0400

    applied Dan's advice regarding the *, thanks

--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -3,23 +3,17 @@ to_sort = [10, 9, 1, 2, 4, 6]
 
 cycle = 3 #variable countergit for the outer loop
 
-while cycle > 0 do #we only need to go through the outter loop once, we will stop once we have executed it once
-   x = 0 #variable for the elements in the array
-    while (x < to_sort.length - 1) do #get the amount of elements in the array and iterate through them
-       if to_sort[x] > to_sort[x+1] #compare if the 2 selected values are in order
-        # y = to_sort[x]
-        # to_sort << y
-        # to_sort.shift[x]
-        to_sort[x], to_sort[x+1] = to_sort[x+1], to_sort[x] #if the 1st value is greater than the 2nd value then swap values
-        sleep 2
-        # a = (1..30000000).to_a #slow down the loop a bit for dramatic effect!
-        # a
-        puts "**********RESORTING**************" #puts some feedback on the screen
-        puts to_sort.join ',' #puts all the values of the array on one line instead of 1 per line 
-        next
-        else 
-      end 
-    x +=1
+  while cycle > 0 do #we only need to go through the outter loop once, we will stop once we have executed it once
+       x = 0 #variable for the elements in the array
+        while (x < to_sort.length - 1) do #get the amount of elements in the array and iterate through them
+           if to_sort[x] > to_sort[x+1] #compare if the 2 selected values are in order
+              to_sort[x], to_sort[x+1] = to_sort[x+1], to_sort[x] #if the 1st value is greater than the 2nd value then swap values
+              sleep 2
+              puts "*" * 20 + "RESORTING" + "*" * 20 #puts some feedback on the screen
+              puts to_sort.join ',' #puts all the values of the array on one line instead of 1 per line 
+             else 
+        end 
+         x +=1
+      end
+      cycle -= 1 #decrease the outter counter value so that we only go through the outter loop once
   end
-cycle -= 1 #decrease the outter counter value so that we only go through the outter loop once
-end
